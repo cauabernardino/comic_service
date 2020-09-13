53debf904170a1d7a09f24907e1e4ae08924f48a
Cauã Bernardino <cbernardino@live.com>
2020-09-13 16:46:52 -0300

    Refactoring of pythons script to check for same comic names. Inserted a blank line in shell script to make clear final crontable.

--- addservice.sh
+++ addservice.sh
@@ -10,11 +10,12 @@ echo "Making the script executable..."
 chmod +x $PWD/comic_service.py;
 
 echo "Appending (or creating) crontable...";
-cronjob="* * * * * $PWD/comic_service.py > /dev/null 2>&1";
+cronjob="0 * * * * $PWD/comic_service.py > /dev/null 2>&1";
 crontab -l > tmpfile;
 echo "$cronjob" >> tmpfile; 
 crontab tmpfile;
 rm tmpfile;
 
-echo "The current crontab is";
+echo
+echo "The current crontable is now:";
 crontab -l;
--- comic_service.py
+++ comic_service.py
@@ -6,25 +6,40 @@ import subprocess
 from bs4 import BeautifulSoup
 
 
-def scraper():
+def scraper(directory):
+    """
+    Gets an url for a comic that was not downloaded recently.
+    """
+    # Get new comic url
     source = requests.get('https://c.xkcd.com/random/comic/').text
     soup = BeautifulSoup(source, features="html.parser")
     div = soup.find('div', id="comic")
-
-    comic_url = 'https:' + div.img['src']
     
-    return comic_url
-
+    # Checks if it's a new one, then returns url
+    check_comic = div.img['src'].split('/')
+    
+    if check_comic[-1] not in os.listdir(directory):
+        comic_url = 'https:' + div.img['src']
+       
+        return comic_url
+    else:
+        scraper(directory)
+      
 
 def get_comics(directory, comic_url):
-    
+    """
+    Downloads comic to the given directory.
+    """
     subprocess.run(['wget', '-P', directory, comic_url])
 
     return 0
 
 
-def check_files(directory):
-
+def check_dir(directory, qty=2):
+    """
+    Checks for the chosen quantity of files in the given directory.
+    The 'qty' parameters is set to 2 by default.
+    """
     # Initialization of dict to store values
     files = {}
 
@@ -39,18 +54,21 @@ def check_files(directory):
     # Let maximum 2 files, delete oldest one
     sorted_files = sorted(files.items(), key=lambda x: x[1], reverse=True)
     
-    while len(sorted_files) >= 2:   
+    while len(sorted_files) >= qty:   
         file_to_remove = sorted_files[-1]
         os.remove(file_to_remove[0])
         sorted_files.remove(file_to_remove)
 
     return 0
-
-
+    
 
 if __name__ == "__main__":
-    dir = os.path.join(os.environ['HOME'], 'comicvault')
-    comic = scraper()
-
-    check_files(dir)
+    # Define directory
+    dir = os.path.join(os.environ['HOME'], 'comicvault') 
+    
+    # New comic url to download
+    comic = scraper(dir)
+    
+    # Checking and downloading
+    check_dir(dir)
     get_comics(dir, comic)
